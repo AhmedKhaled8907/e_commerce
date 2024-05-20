d03e1faaf4721832d2ca81a361aadf512b6c7947
Ahmed khaled <ahmedkh8907@gmail.com>
2024-05-21 02:36:12 +0300

    sign in with google

--- lib/widgets/auth/google_button.dart
+++ lib/widgets/auth/google_button.dart
@@ -1,10 +1,39 @@
+import 'package:e_commerce/screens/root_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 
 import '../title_text.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleButton extends StatelessWidget {
   const GoogleButton({super.key});
+
+  Future<void> signInWithGoogle(context) async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    if (googleUser == null) {
+      return;
+    }
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    await FirebaseAuth.instance.signInWithCredential(credential);
+
+    if (context.mounted) {
+      await Navigator.of(context).pushReplacementNamed(RootScreen.routeName);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +50,9 @@ class GoogleButton extends StatelessWidget {
         label: 'Login with Google',
         color: Colors.white,
       ),
-      onPressed: () {},
+      onPressed: () {
+        signInWithGoogle(context);
+      },
     );
   }
 }
